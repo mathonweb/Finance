99bcc84aa1b6a949ad77a5b66b2bd07dfd556745
Mathieu Guilbault <mguilbault@gmail.com>
2021-11-14 17:11:14 -0500

    Create annual_total_return.csv and send it to S3

--- finance.py
+++ finance.py
@@ -118,8 +118,10 @@ def main():
     finance = Finance()
     finance.create_total_annual_return()
     total_return_df = finance.get_total_annual_return()
+    total_return_df.to_csv("annual_total_return.csv", index=False, index_label=False)
+    send_file("annual_total_return.csv")
 
-    create_total_return_report(total_return_df)
+    # create_total_return_report(total_return_df)
 
 
 if __name__ == '__main__':
