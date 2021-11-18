83f7b25a42816868fc44a532e970f8656cc0c36a
Mathieu Guilbault <mguilbault@gmail.com>
2021-11-18 18:58:09 -0500

    Create a html report from python

--- report.py
+++ report.py
@@ -1,18 +1,48 @@
-from fpdf import FPDF
+import pandas as pd
+
+from utils.s3_client import get_file, send_file
+
+
+class Figures:
+    def __init__(self):
+        self._annual_total_return = self._get_annual_total_return()
+
+    def _get_annual_total_return(self):
+        filename = get_file("annual_total_return.csv")
+        return pd.read_csv(filename)
+
+    def create_annual_return_graph(self):
+        pass
+
+    def create_annual_return_table(self):
+        return self._annual_total_return.to_html(index=False)
 
 
 class Report:
     def __init__(self):
-        self._report = None
+        self.figures = Figures()
 
-    def create_template_report(self):
-        self._report = FPDF(orientation='P', unit='mm', format='A4')
-        self._report.add_page()
-        self._report.set_font("Arial", "B", 16)
-        self._report.cell(40, 10, "Hello World !")
-        self._report.output("financial_report.pdf", "F")
+    def create_report(self):
+
+        f = open("financial_report.html", "w")
+
+        report = "<html>" + \
+                 " <head> <title>Rapport de finance</title> </head>" +\
+                 " <body> <h2>Annual Total Return</h2>" + \
+                 self.figures.create_annual_return_table() + \
+                 "</body>" + \
+                 "</html>"
+
+        f.write(report)
+
+        f.close()
+
+    def send_report(self):
+        send_file("financial_report.html")
 
 
 if __name__ == '__main__':
+
     report = Report()
-    report.create_template_report()
+    report.create_report()
+    report.send_report()
